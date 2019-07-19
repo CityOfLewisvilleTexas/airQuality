adce470d28691163ffa0239d7626ba55f1db4fe1
Holmesc16 <holmesc16@gmail.com>
2019-07-19 09:27:24 -0500

    get payload from backend

--- src/components/AirQuality/index.js
+++ src/components/AirQuality/index.js
@@ -1,6 +1,24 @@
 import React, { useState, useEffect } from "react";
-import { apiURL, setColor } from "../../utils";
+import { setColor, serviceURL } from "../../utils";
 import CustomLoader from '../../ui/Loader'
+
+const useServiceFetch = url => {
+  const [data, setData] = useState(null)
+  const [loading, setLoading] = useState(true)
+
+  async function fetchData() {
+    const response = await fetch(url)
+    const json = await response.json()
+    setData(json[0])
+    console.log(data)
+    setLoading(false)
+  }
+  useEffect(() => {
+    fetchData()
+  }, [url]);
+
+  return {loading, data}
+}
 
 const useFetch = url => {
   const [data, setData] = useState(null);
@@ -21,16 +39,17 @@ const useFetch = url => {
 };
 
 const AirQuality = props => {
-  const {loading, data} = useFetch(apiURL);
+  const {loading, data} = useServiceFetch(serviceURL)
+  console.log(data)
 
   return (
       <div>
           { loading ? <CustomLoader/> : 
-            <div style={{'backgroundColor': setColor(data.Category.Number), 'color': data.Category.Number === 2 ? '#000' : 'inherit'}}>
-                <p>{data.DateForecast}</p>
-                <p>{data.Category.Number}</p>
-                <p>{data.Category.Name}</p>
-                <p>{data.Discussion}</p>
+            <div style={{'backgroundColor': setColor(data[0]["CategoryNumber"]), 'color': data[0]["CategoryNumber"] === 2 ? '#000' : 'inherit'}}>
+                <p>{data[0]["DateForecast"]}</p>
+                <p>{data[0]["CategoryNumber"]}</p>
+                <p>{data[0]["CategoryName"]}</p>
+                <p>{data[0]["Discussion"]}</p>
             </div>
         }
 
